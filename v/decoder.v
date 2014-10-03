9cf78be902986abc646cc7958c4f31325dcc8e39
gillbertS <ldes89150@gmail.com>
2014-10-03 14:12:32 +0800

    add decoder

---
+++ v/decoder.v
@@ -0,0 +1,27 @@
+module decoder
+{
+    iRst,
+    imData,
+    imData_Ready,
+    oCode,
+    oCode_Ready
+
+};
+
+
+// ==== I/O, Reg, Wire Declaration ======================================
+input       [7:0]               imData;
+input                           imData_Ready;
+
+
+
+output      [7:0]               oCode;
+output                          oCode_Ready;
+                
+
+// ==== Structural Design ==============================
+
+
+
+
+endmodule
